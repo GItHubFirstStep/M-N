4d74d88e20a2ae70b7ac18446c3f73d5979aaaa2
GitHubFirstStep <1031404314@qq.com>
2020-12-21 13:20:37 +0800

    A+1

--- M+N/Form1.cs
+++ M+N/Form1.cs
@@ -16,5 +16,10 @@ namespace M_N
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "A+1";
+        }
     }
 }
